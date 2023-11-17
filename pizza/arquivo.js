b9a595e1ca9c622afcda2bf63f60d7bc61ce202b
Brenda Gomes de Almeida <126163627+oajurbd@users.noreply.github.com>
2023-11-17 10:36:26 -0300

    Add files via upload

---
+++ pizza/arquivo.js
@@ -0,0 +1,91 @@
+
+function clicou1(){
+    var num1=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num1*39
+
+        window.alert(multiplicação)
+        var o=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou2(){
+    var num2=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num2*35
+       
+        window.alert(multiplicação)
+        var a=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou3(){
+    var num3=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num3*30
+       
+        window.alert(multiplicação)
+        var b=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou4(){
+    var num4=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num4*25
+       
+        window.alert(multiplicação)
+        var c=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou5(){
+    var num5=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num5*50
+       
+        window.alert(multiplicação)
+        var d=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou6(){
+    var num6=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num6*45
+       
+        window.alert(multiplicação)
+        var e=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou7(){
+    var num7=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num7*24
+       
+        window.alert(multiplicação)
+        var f=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou8(){
+    var num8=Number(window.prompt
+        ("Quanto pratos vc deseja?"))
+        var multiplicação=num8*45
+       
+        window.alert(multiplicação)
+        var o=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+        
+        
---
+++ pizza/arquivooo.js
@@ -0,0 +1,101 @@
+
+function clicou1(){
+    var num1=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num1*10
+        
+        window.alert(multiplicação)
+        var c=Number(window.prompt
+            ("Qual  marca?")) 
+    var o=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue")        
+}
+
+function clicou2(){
+    var num2=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num2*5
+       
+        window.alert(multiplicação)
+        var b=Number(window.prompt
+            ("Qual  sabor?")) 
+        var a=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou3(){
+    var num3=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num3*10
+       
+        window.alert(multiplicação)
+        var d=Number(window.prompt
+            ("Qual  marca?")) 
+        var b=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou4(){
+    var num4=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num4*25
+       
+        window.alert(multiplicação)
+        var e=Number(window.prompt
+            ("Qual  marca?")) 
+        var c=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou5(){
+    var num5=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num5*12
+       
+        window.alert(multiplicação)
+        var h=Number(window.prompt
+            ("Qual  sabor?")) 
+        var d=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou6(){
+    var num6=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num6*3
+       
+        window.alert(multiplicação)
+        
+        var e=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou27(){
+    var num7=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num7*12
+       
+        window.alert(multiplicação)
+        var f=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
+
+function clicou8(){
+    var num8=Number(window.prompt
+        ("Quanto vc deseja?"))
+        var multiplicação=num8*30
+       
+        window.alert(multiplicação)
+        var t=Number(window.prompt
+            ("Qual  tipo?")) 
+        var g=Number(window.prompt
+            ("Qual o número da sua mesa?"))   
+            window.alert("seu pedido será entregue") 
+}
